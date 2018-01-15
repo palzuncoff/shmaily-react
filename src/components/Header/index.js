d6c19cdbe37184bc78406260f1167799e6eea493
Dmitrii Lazucov <palzuncoff@MacBook-Dmitrii.local>
2018-01-15 21:22:04 +0200

    Add buttons on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './index.css';
 
 export class Header extends Component {
     render() {
         return (
-            <h1>Header</h1>
+            <div>
+                <h1>Header</h1>
+                <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+                    From Down
+                </Button>
+                <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+                    From Top
+                </Button>
+                <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+                    From Shuffle
+                </Button>
+            </div>
         );
     };
 };
