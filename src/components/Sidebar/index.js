ab5c51bc51a44c271bb652fbe8108dfc5a687310
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-17 12:17:50 +0200

    add sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import './index.css';
-import { CreatePostForm } from '../CreatePostForm';
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 export class Sidebar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      links: [
+        {
+          name: 'Pictures',
+          url: '/pictures'
+        },
+        {
+          name: 'Videos',
+          url: '/videos'
+        },
+        {
+          name: 'Coubs',
+          url: '/coubs'
+        },
+        {
+          name: 'Articles',
+          url: '/articles'
+        },
+      ]
+    }
+  }
     render() {
         return (
-            <div className="col-3">
-                <h1>Sidebar</h1>
-                <CreatePostForm />
-            </div>
+          <div className="col-3">
+            <aside className="sidebar">
+              <nav className="nav">
+                <Router>
+                <ul className="nav__list">
+                  {this.state.links.map(link =>
+                    <li className="nav__li" key={link.name}>
+                      <Link to={link.url} className="nav__a">{link.name}</Link>
+                    </li>
+                  )}
+                </ul>
+              </Router>
+              </nav>
+            </aside>
+          </div>
         );
     };
 };
