691c555a6666cd71b07cd2f7eeb295bfb095c054
Mihail Sitnic <mihail.sitnic@gmail.com>
2018-01-17 12:11:15 +0200

    add header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
 import './index.css';
+import logo from './img/logo.png';
 
 export class Header extends Component {
+  constructor() {
+    super()
+    this.state = {
+      buttons: [
+        {
+          name: 'From Down',
+          title: 'test'
+        },
+        {
+          name: 'From Top',
+          title: 'test'
+        },
+        {
+          name: 'From Shuffle',
+          title: 'test'
+        }
+      ]
+    }
+  }
     render() {
         return (
-            <header className="header">
+          <header className="header">
             <div className="wrapper">
               <div className="header-wrap">
-                <h1>Header</h1>
-                <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
-                    From Down
-                </Button>
-                <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
-                    From Top
-                </Button>
-                <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
-                    From Shuffle
-                </Button>
+                <a href="/" className="logo">
+                  <img src={logo} className="logo-img" />
+                </a>
+                <nav className="header-nav">
+                  <ul className="header-list">
+                    {this.state.buttons.map(bt =>
+                      <li className="header-li" key={bt.name}>
+                        <button className="header-bt" title={bt.title}>{bt.name}</button>
+                      </li>
+                    )}
+                  </ul>
+                </nav>
               </div>
             </div>
           </header>
