0f554b317a4d7cf84e4af67173f32c5271f4856c
Mihail Ciobanu <positronmihai@gmail.com>
2018-01-18 17:54:52 +0200

    Header-buttons
    Changed names of the header buttons, gave them styles and fancy titles if you hover over.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,13 +14,13 @@ export class Header extends Component {
                         <nav className="header-nav">
                             <ul className="header-list">
                                 <li className="header-li">
-                                    <button className="header-bt" title="test">From Down</button>
+                                    <button className="header-bt" title="Brand new stuff">Recent</button>
                                 </li>
                                 <li className="header-li">
-                                    <button className="header-bt" title="test">From Top</button>
+                                    <button className="header-bt" title="Posts appear in random order">Randomize</button>
                                 </li>
                                 <li className="header-li">
-                                    <button className="header-bt" title="test">From Shuffle</button>
+                                    <button className="header-bt" title="Start all over from the beginning of time">Back to the future</button>
                                 </li>
                             </ul>
                         </nav>
