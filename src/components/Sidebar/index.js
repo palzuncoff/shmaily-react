e0422119530b72709ec77b2ad5bf68bea16ccff0
Nicolai Voronin <nicolai.voronin@gmail.com>
2018-01-25 21:16:36 +0200

    Create route for CreatePostForm

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import { CreatePostForm } from '../CreatePostForm';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 export class Sidebar extends Component {
@@ -9,28 +8,25 @@ export class Sidebar extends Component {
             <div className="col-3">
                 <aside className="sidebar">
                     <nav className="nav">
-                        <CreatePostForm />
-                        <Router>
-                            <ul className="nav__list">
-                                <li className="nav__li">
-                                    <Link to="/pictures" className="nav__a">Pictures</Link>
-                                </li>
-                                <li className="nav__li">
-                                    <Link to="/videos" className="nav__a">Videos</Link>
-                                </li>
-                                <li className="nav__li">
-                                    <Link to="/coubs" className="nav__a">Coubs</Link>
-                                </li>
-                                <li className="nav__li">
-                                    <Link to="/articles" className="nav__a">Articles</Link>
-                                </li>
-                            </ul>
-                        </Router>
+                        <ul className="nav__list">
+                            <li className="nav__li">
+                                <Link to="/pictures" className="nav__a">Pictures</Link>
+                            </li>
+                            <li className="nav__li">
+                                <Link to="/videos" className="nav__a">Videos</Link>
+                            </li>
+                            <li className="nav__li">
+                                <Link to="/coubs" className="nav__a">Coubs</Link>
+                            </li>
+                            <li className="nav__li">
+                                <Link to="/articles" className="nav__a">Articles</Link>
+                            </li>
+                        </ul>
                     </nav>
                 </aside>
             </div>
         );
     };
-};
+}
 
 export default Sidebar;
