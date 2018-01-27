60aa75f9bd3bfeef2468c48d0915fa3309fa577e
Dmitrii Lazucov <palzuncoff@MacBook-Dmitrii.local>
2018-01-27 15:14:58 +0200

    Fix linter warning

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,13 +14,22 @@ export class Header extends Component {
                         <nav className="header-nav">
                             <ul className="header-list">
                                 <li className="header-li">
-                                    <button className="header-bt" title="Brand new stuff">Recent</button>
+                                    <button
+                                        className="header-bt"
+                                        title="Brand new stuff"
+                                    >Recent</button>
                                 </li>
                                 <li className="header-li">
-                                    <button className="header-bt" title="Posts appear in random order">Randomize</button>
+                                    <button
+                                        className="header-bt"
+                                        title="Posts appear in random order"
+                                    >Randomize</button>
                                 </li>
                                 <li className="header-li">
-                                    <button className="header-bt" title="Start all over from the beginning of time">Back to the future</button>
+                                    <button
+                                        className="header-bt"
+                                        title="Start all over from the beginning of time"
+                                    >Back to the future</button>
                                 </li>
                             </ul>
                         </nav>
